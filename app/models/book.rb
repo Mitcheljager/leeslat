65aa4f2f677a2f5ea6136dd855d1814a79704172
Mitchel <mitchel_jager@hotmail.com>
2025-07-15 21:45:45 +0200

    chore: Add validations to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,10 @@
 class Book < ApplicationRecord
   self.primary_key = "isbn"
 
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :isbn, presence: true, uniqueness: true
+
   def to_param
     isbn
   end
