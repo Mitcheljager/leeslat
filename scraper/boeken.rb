9a0dad868f2ba0171a52722c432b2ce201c4e841
Mitchel <mitchel_jager@hotmail.com>
2025-07-15 03:32:44 +0200

    fix: Use data-src for image in boeken scraper

--- scraper/boeken.rb
+++ scraper/boeken.rb
@@ -6,7 +6,7 @@ document = get_document("https://www.boeken.nl/boeken/9780007299263/howls-moving
 title = document.css("h1").text.strip
 isbn = document.css(".field-name-field-isbn").first.text.strip
 price = document.css(".product-info .uc-price").first.text.strip
-image = document.css(".group-cover-and-photos .field-name-field-cover img").first.attribute("src").value.strip
+image = document.css(".group-cover-and-photos .field-name-field-cover img").first.attribute("data-src").value.strip
 
 puts title
 puts isbn
