a7123ea67edd6a167a3c96e6cbae215798d393c1
Mitchel <mitchel_jager@hotmail.com>
2025-07-16 21:24:30 +0200

    feat: Add popular list of boeken.nl

--- scraper/get_popular_books.rb
+++ scraper/get_popular_books.rb
@@ -1,13 +1,29 @@
 require_relative "base"
 
-document = get_document("https://www.debestseller60.nl/")
-
 isbn_list = []
 
-document.css(".card__tags__tag").each do |tag|
-  next unless tag.text.include?("ISBN")
+document = get_document("https://www.debestseller60.nl/")
+document.css(".card__tags__tag").each do |node|
+  next unless node.text.include?("ISBN")
 
-  isbn_list << tag.text.gsub("ISBN", "").strip
+  isbn = node.text.gsub("ISBN", "").strip
+  isbn_list << isbn if isbn.present?
+end
+
+document = get_document("https://www.boeken.nl/boeken/top-100?mefibs-form-view-options-top-keys=&mefibs-form-view-options-top-items_per_page=100&mefibs-form-view-options-top-mefibs_block_id=view_options_top")
+document.css(".views-row a").each do |node|
+  url = node.attribute("href").value
+
+  next if url.blank?
+
+  isbn = url.match(%r{/(\d{10,13})/})&.captures&.first
+
+  next if isbn.blank?
+  next if isbn_list.include?(isbn)
+
+  puts "Adding: #{isbn}"
+
+  isbn_list << isbn
 end
 
 isbn_list.each do |isbn|
