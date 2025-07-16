a05c3db88589870d62f1e88d2da35842fbe5eacd
Mitchel <mitchel_jager@hotmail.com>
2025-07-16 22:34:28 +0200

    fix: Use price from search results unless result is listed as ebook

--- scraper/sources/amazon.rb
+++ scraper/sources/amazon.rb
@@ -2,24 +2,36 @@ require_relative "../base"
 require "nokogiri"
 
 def scrape_amazon(isbn)
-  basepath = "https://www.amazon.nl"
-  search_document = get_document("#{basepath}/s?k=#{isbn}")
-  first_search_item_path = search_document.css("[role='listitem'] a").first.attribute("href").value
-  url = basepath + first_search_item_path
-
-  puts "Running Amazon for: " + url
+  base_path = "https://www.amazon.nl"
+  url = "#{base_path}/s?k=#{isbn}"
 
   document = get_document(url)
 
-  price_text = document.at_css("#tmm-grid-swatch-PAPERBACK")&.text
-  price = price_text.to_s.gsub(/[[:space:]]/, "").gsub("€", "").strip.gsub(",", ".").gsub("Paperback", "").strip
+  puts "Running Amazon for: " + url
 
-  if price.blank?
-    price_text = document.at_css(".priceToPay span")&.text
-    price = price_text.to_s.gsub("€", "").gsub(",", ".").strip
+  is_ebook = document.at_css(".s-price-instructions-style")&.text.include?("Kindle")
+
+  if is_ebook
+    puts "Search price was listed as ebook, entering page instead"
+
+    first_search_item_path = document.css("[role='listitem'] a").first.attribute("href").value
+    document = get_document(base_path + first_search_item_path)
+
+    price_text = document.at_css("#tmm-grid-swatch-PAPERBACK")&.text
+    price = price_text.to_s.gsub(/[[:space:]]/, "").gsub("€", "").strip.gsub(",", ".").gsub("Paperback", "").strip
+
+    if price.blank?
+      price_text = document.at_css(".priceToPay span")&.text
+      price = price_text.to_s.gsub("€", "").gsub(",", ".").strip
+    end
+
+    image = document.css("#landingImage").first.attribute("src").value.strip
+  else
+    price_text = document.at_css(".a-price .a-offscreen")&.text
+    price = price_text.to_s.gsub(/[[:space:]]/, "").gsub("€", "").gsub(",", ".").strip
+
+    image = document.css(".s-image").first.attribute("src").value.strip
   end
-
-  image = document.css("#landingImage").first.attribute("src").value.strip
 
   puts isbn
   puts price
