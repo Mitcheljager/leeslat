1ea52043cf051dd072251b04d87a72edc3996c6c
Mitchel <mitchel_jager@hotmail.com>
2025-07-17 19:36:54 +0200

    refactor: Move google api to separate file

--- scraper/base.rb
+++ scraper/base.rb
@@ -1,4 +1,5 @@
 require_relative "../config/environment"
+require_relative "data/google_api"
 require "httparty"
 require "nokogiri"
 
@@ -84,24 +85,3 @@ def find_listing_for_isbn_and_source_name(isbn, source_name)
   listing = book&.listings&.joins(:source)&.find_by(sources: { name: source_name })
 end
 
-def get_google_api_data(isbn)
-  google_api_url = "https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}"
-
-  puts "Running Google API for: #{google_api_url}"
-
-  book_data_response = HTTParty.get(google_api_url)
-  parsed_response = JSON.parse(book_data_response.body)
-
-  return if parsed_response["totalItems"] === 0 || parsed_response["items"].blank?
-
-  item = parsed_response["items"][0]
-  volume_info = item["volumeInfo"]
-  sale_info = item["saleInfo"]
-
-  is_ebook = sale_info["isEbook"] == true
-  title = volume_info["title"]
-  language = volume_info["language"]
-  authors = volume_info["authors"]
-
-  [is_ebook, title, language, authors]
-end
---
+++ scraper/data/google_api.rb
@@ -0,0 +1,23 @@
+require "httparty"
+
+def get_google_api_data(isbn)
+  google_api_url = "https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}"
+
+  puts "Running Google API for: #{google_api_url}"
+
+  book_data_response = HTTParty.get(google_api_url)
+  parsed_response = JSON.parse(book_data_response.body)
+
+  return if parsed_response["totalItems"] === 0 || parsed_response["items"].blank?
+
+  item = parsed_response["items"][0]
+  volume_info = item["volumeInfo"]
+  sale_info = item["saleInfo"]
+
+  is_ebook = sale_info["isEbook"] == true
+  title = volume_info["title"]
+  language = volume_info["language"]
+  authors = volume_info["authors"]
+
+  [is_ebook, title, language, authors]
+end
