9708957be12da50b3972616ac85a3031dcef2f43
Mitchel <mitchel_jager@hotmail.com>
2025-07-17 19:58:32 +0200

    feat: Add basic goodreads scraper
    For now only returning genres and format

---
+++ scraper/data/goodreads.rb
@@ -0,0 +1,22 @@
+require_relative "../base"
+require "nokogiri"
+
+def get_goodreads_data(isbn)
+  goodreads_search_url = "https://www.goodreads.com/search?q=#{isbn}"
+
+  puts "Running Goodreads for: #{goodreads_search_url}"
+
+  document = get_document(goodreads_search_url)
+  genres = document.css("[data-testid='genresList'] a").map(&:text)
+  format_text = document.css("[data-testid='pagesFormat']").text
+
+  format = "unknown"
+  format = "paperback" if format_text.include?("Paperback")
+  format = "hardcover" if format_text.include?("Hardcover")
+
+  [genres, format]
+end
+
+isbn = ARGV[0]
+
+get_goodreads_data(isbn) if isbn
