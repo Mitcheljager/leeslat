30cf7c9bd562acdeb3e91b057ac7b7564c305219
Mitchel <mitchel_jager@hotmail.com>
2025-07-19 22:08:27 +0200

    chore: Formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite"
-import RubyPlugin from "vite-plugin-ruby"
-import FullReload from "vite-plugin-full-reload"
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
+import FullReload from "vite-plugin-full-reload";
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    FullReload(["config/routes.rb", "app/views/**/*", "app/assets/**/*"]),
-  ],
-})
+    FullReload(["config/routes.rb", "app/views/**/*", "app/assets/**/*"])
+  ]
+});
