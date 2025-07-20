e7947e27fb0200c82d7fd5116d77cff882bfb7e5
Mitchel <mitchel_jager@hotmail.com>
2025-07-20 16:31:58 +0200

    chore: Adjust ai descriptions prompt
    It's not performing all that well :(

--- scraper/ai/openai_descriptions.rb
+++ scraper/ai/openai_descriptions.rb
@@ -23,14 +23,10 @@ response = client.chat(parameters: {
   model: "gpt-3.5-turbo",
   messages: [{
     role: "system",
-    content: "Your task is to clean up and merge a set of book descriptions you are given. The descriptions will be in Dutch or English and should be returned in the language they were given. The descriptions might include quotes, blurb or reviews, which should be removed. The only thing that should remain is a description of the book, without any extras. The description should be close to the original texts without rewording. If different given descriptions contain vastly different text, they should all be included. It's all about cleaning up and concatenating, rather than rewording. Grammatical or formatting errors should be fixed."
+    content: "Your task is to clean up and merge a set of book descriptions you are given. The descriptions will be in Dutch or English and should be returned in the language they were given. The descriptions might include quotes, blurbs, or reviews, which should be removed. The only thing that should remain is a description of the book, without any extras. The description should be close to the original texts without rewording. If different given descriptions contain vastly different text, they should all be included. It's all about cleaning up and concatenating. Grammatical or formatting errors should be fixed, including missing spaces or new lines."
   }, {
     role: "user",
-    content: "Please clean up and merge these book descriptions, removing reviews, blurbs, quotes, mentions of the author and translator, and mentions of the edition of this book:\n
-    - Any text mentioning awards or bestseller statuses should be removed.\n
-    - Any text mentioning the edition of the book should be removed.\n
-    - Any text regarding the author or translator should be removed.\n
-    - Any blurbs or opinions about the book or author should be removed.\n
+    content: "Please clean up and merge these book descriptions. Please removing reviews, blurbs, quotes, mentions of the author and translator, and mentions edition of this book. Additionally, please fix any grammar or formatting issues or inconsistencies. Insert new lines where relevant:\n
     \n\n" + descriptions.join("\n\n")
   }],
   temperature: 0.7,
