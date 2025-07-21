5347c2088b7a1bdd61079376f08872bb42c21636
Mitchel <mitchel_jager@hotmail.com>
2025-07-21 14:15:40 +0200

    chore: Adjust page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def human_list(array)
+    if array.empty?
+      ""
+    elsif array.length == 1
+      array.first.to_s
+    elsif array.length == 2
+      "#{array[0]} en #{array[1]}"
+    else
+      "#{array[0...-1].join(", ")} en #{array[-1]}"
+    end
+  end
 end
