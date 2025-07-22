ad9f6d4aad5a0f3efc014d82e320797fbcaa64a4
Mitchel <mitchel_jager@hotmail.com>
2025-07-22 22:21:08 +0200

    style: Add listing layout

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,4 +8,12 @@ class Listing < ApplicationRecord
 
   validates :currency, inclusion: { in: %w[EUR USD GBP], message: "%{value} is not a valid currency" }, allow_nil: true
   validates :condition, inclusion: { in: Listing.conditions.keys }
+
+  def price_large
+    self.price.to_s.split(".")[0]
+  end
+
+  def price_cents
+    self.price.to_s.split(".")[1]
+  end
 end
