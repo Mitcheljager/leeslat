1e427ace669d9007eb93b7a8ad653daa4d5057a9
Mitchel <mitchel_jager@hotmail.com>
2025-07-25 20:09:48 +0200

    refactor: Use static variables in theme_toggle
    Should result in fewer (none) variables being passed around, but does it make it easier to read? Not entirely sure just yet.

--- app/frontend/javascript/theme_toggle.ts
+++ app/frontend/javascript/theme_toggle.ts
@@ -5,52 +5,55 @@ import { prefers_reduced_motions } from "./utilities/prefers_reduced_motion";
 type Theme = "light" | "dark";
 
 export default class theme_toggle {
+  static theme: Theme;
+  static html_element: HTMLHtmlElement;
+
   bind(): void {
     const toggle_button: HTMLButtonElement | null = document.querySelector("[data-action~='toggle_theme']");
 
     if (!toggle_button) return;
 
-    const html_element =  document.querySelector("html") as HTMLHtmlElement;
-    const theme = this.get_initial_theme(html_element);
+    theme_toggle.html_element = document.querySelector("html") as HTMLHtmlElement;
+    theme_toggle.theme = this.get_initial_theme();
 
-    html_element.style.setProperty("color-scheme", theme);
-    html_element.style.viewTransitionName = "changing-theme";
+    theme_toggle.html_element.style.setProperty("color-scheme", theme_toggle.theme);
+    theme_toggle.html_element.style.viewTransitionName = "changing-theme";
 
-    this.set_clip_path_target(html_element, theme);
-    this.set_cookie(theme);
+    this.set_clip_path_target();
+    this.set_cookie();
 
-    if (theme === "dark") new switch_toggle().toggle(toggle_button, true);
+    if (theme_toggle.theme === "dark") new switch_toggle().toggle(toggle_button, true);
 
     toggle_button.addEventListener("click", () => {
       setTimeout(() => {
         fallback_view_transition(() => {
-          const currentTheme = html_element.style.getPropertyValue("color-scheme") as Theme;
-          const theme = currentTheme === "light" ? "dark" : "light";
+          const currentTheme = theme_toggle.html_element.style.getPropertyValue("color-scheme") as Theme;
+          theme_toggle.theme = currentTheme === "light" ? "dark" : "light";
 
-          this.set_clip_path_target(html_element, theme);
-          html_element.style.setProperty("color-scheme", theme);
+          this.set_clip_path_target();
+          theme_toggle.html_element.style.setProperty("color-scheme", theme_toggle.theme);
 
-          this.set_cookie(theme);
+          this.set_cookie();
         }, !prefers_reduced_motions());
       }, window.ViewTransition ? 200 : 0);
     });
   }
 
-  get_initial_theme(html_element: HTMLHtmlElement): Theme {
-    const html_theme = html_element.style.getPropertyValue("color-scheme");
+  private get_initial_theme(): Theme {
+    const html_theme = theme_toggle.html_element.style.getPropertyValue("color-scheme");
     const window_theme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
 
     return (html_theme || window_theme) as Theme;
   }
 
-  set_clip_path_target(html_element: HTMLHtmlElement, theme: Theme): void {
+  private set_clip_path_target(): void {
     const light = "0 0, 100% 0%, 100% 0, 0 0";
     const dark = "0 100%, 100% 100%, 100% 100%, 0 100%";
 
-    html_element.style.setProperty("--theme-toggle-clip-path", theme === "dark" ? dark : light);
+    theme_toggle.html_element.style.setProperty("--theme-toggle-clip-path", theme_toggle.theme === "dark" ? dark : light);
   }
 
-  set_cookie(theme: Theme): void {
-    document.cookie = `theme=${theme}; path=/; max-age=31536000`;
+  private set_cookie(): void {
+    document.cookie = `theme=${theme_toggle.theme}; path=/; max-age=31536000`;
   }
 }
