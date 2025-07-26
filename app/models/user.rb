a3f39f14d68b8408d0bc9e29692c7ea189e30e55
Mitchel <mitchel_jager@hotmail.com>
2025-07-26 12:38:31 +0200

    chore: Set minimum of 4 characters for user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 
   has_many :remember_tokens, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[\p{L}\d_-]*\z/u }, length: { maximum: 32 }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[\p{L}\d_-]*\z/u }, length: { maximum: 32, minimum: 4 }
   validates :password, presence: true, length: { minimum: 8 }
 end
