2a41e47d46ac17562713a80df2f078a651fd2ee2
Mitchel <mitchel_jager@hotmail.com>
2025-07-26 12:40:54 +0200

    fix: Only validate password if given

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   has_many :remember_tokens, dependent: :destroy
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[\p{L}\d_-]*\z/u }, length: { maximum: 32, minimum: 4 }
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 }, if: :password
 end
