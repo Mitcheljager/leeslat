b9f8a81dca0d5f336874c11ec39ef06628cb0ef5
Mitchel <mitchel_jager@hotmail.com>
2025-07-27 21:04:49 +0200

    feat: Show progress bar when using turbo frames

--- app/frontend/entrypoints/application.ts
+++ app/frontend/entrypoints/application.ts
@@ -3,11 +3,13 @@ import theme_toggle from "../javascript/theme_toggle";
 import switch_toggle from "../javascript/switch_toggle";
 import expand_text from "../javascript/expand_text";
 import poll_listings_summary from "../javascript/poll_listings_summary";
+import turbo_action_progress_bar from "../javascript/turbo_action_progress_bar";
 
 document.addEventListener("turbo:load", () => {
   new theme_toggle().bind();
   new switch_toggle().bind();
   new expand_text().bind();
+  new turbo_action_progress_bar().bind();
 
   new poll_listings_summary().run();
 });
---
+++ app/frontend/javascript/turbo_action_progress_bar.ts
@@ -0,0 +1,26 @@
+import { Turbo } from "@hotwired/turbo-rails";
+
+/**
+ * data-turbo-action can be used to set the url when navigating with Turbo frames. It doesn't
+ * actually show the progress bar when this happen. This class takes care of that by binding
+ * events to form submissions and checking if the event was from a data-turbo-action element.
+ * https://github.com/hotwired/turbo/issues/540#issuecomment-1783752175
+ */
+export default class turbo_action_progress_bar {
+  bind(): void {
+    const adapter = Turbo.navigator.delegate.adapter;
+
+    document.addEventListener("turbo:before-fetch-request", (event) => {
+      const target = event.target;
+
+      if (!(target instanceof HTMLElement)) return;
+      if (target.getAttribute("data-turbo-action") !== "advance") return;
+
+      adapter.formSubmissionStarted();
+    });
+
+    document.addEventListener("turbo:before-fetch-response", () => {
+      adapter.formSubmissionFinished();
+    });
+  }
+}
