4dbc82b14b29a8e0e53b38fb19436744f3e66fcb
Mitchel <mitchel_jager@hotmail.com>
2025-07-27 21:31:34 +0200

    refactor: Use simpler routes to always have books_path rather than books_root_path

--- config/routes.rb
+++ config/routes.rb
@@ -17,11 +17,12 @@ Rails.application.routes.draw do
   get "logout", to: "sessions#destroy", as: "logout"
 
   # Books and related
-  resources :books, param: :slug_and_isbn, path: "boek", only: [:show], concerns: :paginatable
   resources :authors, path: "naam", param: :slug, only: [:show]
   resources :genres, only: [:show]
 
-  get "boeken", to: "books#index", as: :books_root
+  get "boek/:slug_and_isbn", to: "books#show", as: :book
+  get "boeken", to: "books#index", as: :books, concerns: :paginatable
+
   get "book/listings/:slug_and_isbn", to: "books#listings_summary_partial", as: :book_listings_summary
 
   # Actions
