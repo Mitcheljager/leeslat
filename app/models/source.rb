1e5408361971bb557b45889670b400c3511ccb6d
Mitchel <mitchel_jager@hotmail.com>
2025-07-27 02:00:14 +0200

    feat: Rename logo blob before saving

--- app/models/source.rb
+++ app/models/source.rb
@@ -9,6 +9,15 @@ class Source < ApplicationRecord
   validates :slug, presence: true, uniqueness: { case_sensitive: false }
   validates :base_url, presence: true
 
+  # Update file name of logo to slug + extension before saving.
+  # This is mostly to prevent rogue file names from being left in accidentally.
+  before_save do
+    if logo.attached?
+        ext = "." + logo.blob.filename.extension
+        logo.blob.update(filename: slug + ext)
+    end
+  end
+
   def to_param
     slug
   end
