303b6d79a54ae1b4b0f998b666a33e2af4c6614a
Mitchel <mitchel_jager@hotmail.com>
2025-08-01 21:43:46 +0200

    chore: Add bestseller mentions to excludes in ai descriptions

--- scraper/ai/openai_descriptions.rb
+++ scraper/ai/openai_descriptions.rb
@@ -26,7 +26,7 @@ response = client.chat(parameters: {
     content: "Your task is to clean up and merge a set of book descriptions you are given. The descriptions will be in Dutch or English and should be returned in the language they were given. The descriptions might include quotes, blurbs, or reviews, or release dates, which should all be removed. The only thing that should remain is a description of the book, without any extras. The description should be close to the original texts without rewording. If different given descriptions contain vastly different text, they should all be included. It's all about cleaning up and concatenating. Grammatical or formatting errors should be fixed, including missing spaces or new lines."
   }, {
     role: "user",
-    content: "Please clean up and merge these book descriptions. Please removing awards, reviews, blurbs, quotes, release dates, mentions of the author and translator, and mentions edition of this book. Additionally, please fix any grammar or formatting issues or inconsistencies. Insert new lines where relevant:\n
+    content: "Please clean up and merge these book descriptions. Please remove awards, bestseller mentions, reviews, blurbs, quotes, release dates, mentions of the author and translator, and mentions edition of this book. Additionally, please fix any grammar or formatting issues or inconsistencies. Insert new lines where relevant:\n
     \n\n" + descriptions.join("\n\n")
   }],
   temperature: 0.7
