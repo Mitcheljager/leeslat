80e49de5375b99646fe6a6cbb84b470c929404e9
Mitchel <mitchel_jager@hotmail.com>
2025-08-03 22:27:39 +0200

    feat: Add subgenres with books to genres show page

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,11 +2,13 @@ class GenresController < ApplicationController
   before_action :set_genre, only: [:show]
 
   def show
+    @hot_books = @genre.books.order(hotness: :desc).limit(8)
+    @subgenres = @genre.subgenres.includes(:books).left_joins(:books).group("genres.id").order("COUNT(books.id) DESC").limit(4)
   end
 
   private
 
   def set_genre
-    @genre = Genre.find_by_slug!(params.expect(:slug))
+    @genre = Genre.includes(:books).find_by_slug!(params.expect(:slug))
   end
 end
