11391b89b79653589ab961be7419075e99b75f6d
Mitchel <mitchel_jager@hotmail.com>
2025-08-03 14:08:37 +0200

    fix: Adjust scores in get_popular_books to actually make sense
    Previously most scores did nothing and adding of scores was skipped if the isbn was already present in the list

--- scraper/get_popular_books.rb
+++ scraper/get_popular_books.rb
@@ -3,31 +3,50 @@ require_relative "base"
 # This is a hash containing all collected ISBNs with a popularity score. Higher == better.
 isbn_list = Hash.new(0)
 
+index = 0
+
 # Bestseller60 - 60 entries
 document = get_document("https://www.debestseller60.nl/")
-document.css(".card__tags__tag").each_with_index do |node, index|
+document.css(".card__tags__tag").each do |node|
   # The selector above contains all sorts of tags, not just ISBNs
   next unless node.text.include?("ISBN")
 
   isbn = node.text.gsub("ISBN", "").strip
   next if isbn.blank?
 
-  score = 60 - index
+  # Score weighs a bit higher than others might since it's limited to 60 entries
+  score = [240 - (index * 4), 1].max
   isbn_list[isbn] += score
+
+  index += 1
 end
 
 # Bol.com - 30 entries per page, 3 pages
-for page in 1..3 do
-  document = get_document("https://www.bol.com/nl/nl/l/boeken/8299/?page=#{page}")
-  document.css(".product-item__content").each do |node|
-    next if node.include?("Gesponsord")
-    next if node.include?("Ebook") # Specifically Ebook, not e-book, as that would include the other variant sections
+# Subpaths are to get Dutch and English books separately. 8292 is Enlgish, 8293 is Dutch
+subpaths = ["8292", "8293"]
+subpaths.each do |path|
+  index = 0
 
-    # Find any 13 digit code, presumably the ISBN
-    match = node.to_s.match(/\b\d{13}\b/)
-    isbn_list[match[0]] += 0 if match
+  for page in 1..3 do
+    # + 11209 is the category for books, which means we exclude Ebooks and Audiobooks
+    document = get_document("https://www.bol.com/nl/nl/l/boeken/8299/#{path}+11209/?page=#{page}")
+    document.css(".product-item__content").each do |node|
+      next if node.include?("Gesponsord")
+      next if node.include?("Ebook") # Specifically Ebook, not e-book, as that would include the other variant sections
+
+      # Find any 13 digit code, presumably the ISBN
+      match = node.to_s.match(/\b\d{13}\b/)
+      next unless match.present?
+
+      score = [200, 1].max
+      isbn_list[match[0]] += score
+
+      index += 1
+    end
   end
 end
+
+index = 0
 
 # Boeken.nl - 100 entries per page, 2 pages
 for page in 1..2 do
@@ -41,11 +60,15 @@ for page in 1..2 do
     isbn = url.match(%r{/(\d{10,13})/})&.captures&.first
 
     next if isbn.blank?
-    next if isbn_list.include?(isbn)
 
-    isbn_list[isbn] += 0
+    score = [200 - (index * 2), 1].max
+    isbn_list[isbn] += score
+
+    index += 1
   end
 end
+
+index = 0
 
 # Bruna.nl - 100 entries
 document = get_document("https://www.bruna.nl/boeken-top-100")
@@ -59,9 +82,11 @@ document.css(".ankeiler-wrapper [data-test-id='titleAndSubtitle'] a").each do |n
   isbn = url.match(/(\d{10,13})(?:$|\D)/)&.captures&.first
 
   next if isbn.blank?
-  next if isbn_list.include?(isbn)
 
-  isbn_list[isbn] += 0
+  score = [200 - (index * 2), 1].max
+  isbn_list[isbn] += score
+
+  index += 1
 end
 
 # Process all indexed ISBNs, skipping any that are invalid
@@ -74,10 +99,6 @@ isbn_list.each do |isbn, score|
     puts "Skipped #{isbn}"
     next
   end
-
-  puts score
-
-  next
 
   begin
     book = get_book(isbn, attach_image: true)
