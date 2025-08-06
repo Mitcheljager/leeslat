e4d902aaf2f88f784abd1de582c4a9baaa82e2ed
Mitchel <mitchel_jager@hotmail.com>
2025-08-06 21:47:58 +0200

    fix(bruna): Fall back to 0 if no price element if found

--- scraper/sources/bruna.rb
+++ scraper/sources/bruna.rb
@@ -16,7 +16,7 @@ def scrape_bruna(isbn, title)
 
   return { url: nil, available: false } if url.blank? || !url.include?("/boeken/") || document.blank?
 
-  price = document.at_css(".price-block .colored").text.strip
+  price = document.at_css(".price-block .colored")&.text&.strip || 0
   description = document.at_css(".description .line-clamp-8")&.text&.split("Veelgestelde vragen")&.first&.strip
   number_of_pages_label = document.at_css(".product-meta-description div:nth-child(3)")
   number_of_pages = number_of_pages_label&.text&.strip
