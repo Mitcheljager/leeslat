ccb6521a0298fa7ad6595e527601888463b79219
Mitchel <mitchel_jager@hotmail.com>
2025-08-07 23:13:55 +0200

    feat: Add caching to index and genre show pages

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,7 +2,7 @@ class GenresController < ApplicationController
   before_action :set_genre, only: [:show]
 
   def show
-    @hot_books = @genre.books.includes(:authors).order(hotness: :desc).limit(8)
+    @hot_books = @genre.books.includes(:authors, :listings).order(hotness: :desc).limit(8)
     @subgenres = @genre.subgenres.order(books_count: :desc).limit(4)
   end
 
