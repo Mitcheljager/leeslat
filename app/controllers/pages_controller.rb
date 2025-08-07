b2f174f0252c8507aa6e171f545f6c5b0d4bec3f
Mitchel <mitchel_jager@hotmail.com>
2025-08-08 00:41:57 +0200

    feat: Add more books to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def index
-    @sources = Source.all
-    @books = Book.order(hotness: :desc).limit(8)
+    @hot_books = Book.order(hotness: :desc).limit(8)
+    @mystery_books = Genre.find_by_slug("mysterie").books.order(hotness: :desc).limit(8)
+    @fantasy_books = Genre.find_by_slug("fantasy").books.order(hotness: :desc).limit(8)
+    @cook_books = Genre.find_by_slug("kookboeken").books.order(hotness: :desc).limit(8)
   end
 
   def about
