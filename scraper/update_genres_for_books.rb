8b3af89782fcad11619991f7ab828768679720df
Mitchel <mitchel_jager@hotmail.com>
2025-08-07 12:41:41 +0200

    feat(scraper): Allow re-running genres for books with genre
    Also add a sleep to not be too eager

--- scraper/update_genres_for_books.rb
+++ scraper/update_genres_for_books.rb
@@ -3,6 +3,7 @@ require_relative "data/goodreads"
 
 arguments = ARGV.map { |a| a.split("=", 2) }.to_h
 isbn = arguments["isbn"]
+with_genre = arguments["with_genre"]
 
 def find_genres_for_book(book)
   data = get_goodreads_data(book.isbn)
@@ -13,7 +14,11 @@ if isbn.present?
   book = Book.find_by_isbn(isbn)
   find_genres_for_book(book)
 else
-  Book.all.each do |book|
+  books = with_genre.present? ? Book.joins(:genres).where(genres: { name: with_genre }) : Book.all
+
+  books.each do |book|
     find_genres_for_book(book)
+
+    sleep(2)
   end
 end
